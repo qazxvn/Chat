d7fbdaf8a69cb63b2bfd1c0edc764ca53de290d5
Gasta144 <Gasta144@DESKTOP-NRGSGTF>
2025-10-17 15:51:02 +0300

    Initial commit: backend + frontend

---
+++ Backend/Chat/Chat/Models/UserConnection.cs
@@ -0,0 +1,7 @@
+﻿namespace Chat.Models;
+
+public class UserConnection
+{
+    public string UserName { get; set; }
+    public string ChatRoom { get; set; }
+}
---
+++ Backend/Chat/Chat/Program.cs
@@ -0,0 +1,30 @@
+using Chat.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddStackExchangeRedisCache(opttions =>
+{
+    var connection = builder.Configuration.GetConnectionString("Redis");
+    opttions.Configuration = connection;
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.UseCors();
+
+app.MapHub<ChatHub>("/chat");
+
+app.Run();
